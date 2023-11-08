a99b76fc321481886ea6db2092616f8d1e581a91
crooz13 <zee.indigo5@gmail.com>
2023-11-08 16:55:49 +0700

    update respon pemesanan di folder pemesanan

--- app/Http/Controllers/API/Dashboard/DashboardMuaController.php
+++ app/Http/Controllers/API/Dashboard/DashboardMuaController.php
@@ -118,31 +118,4 @@ class DashboardMuaController extends Controller
             'data' => $data
         ]);
     }
-
-    public function acceptPemesanan($id)
-    {
-        $pemesanan = Pemesanan::find($id);
-        $pemesanan->status = 'accept';
-        $pemesanan->save();
-
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Pemesanan Telah Di Terima',
-            'data' => $pemesanan,
-        ], 200);
-    }
-
-    public function declinePemesanan($id)
-    {
-        $pemesanan = Pemesanan::find($id);
-        $pemesanan->status = 'decline';
-        $pemesanan->save();
-
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Pemesanan Telah Di Tolak',
-            'data' => $pemesanan,
-        ], 200);
-    }
-    
 }
