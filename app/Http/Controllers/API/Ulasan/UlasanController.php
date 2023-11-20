682e8e5938a39d825010958fc8af7041d562ccf2
crooz13 <zee.indigo5@gmail.com>
2023-11-20 22:01:13 +0700

    tambah ulasan controller dan routenya

--- app/Http/Controllers/API/Ulasan/UlasanController.php
+++ app/Http/Controllers/API/Ulasan/UlasanController.php
@@ -72,15 +72,46 @@ class UlasanController extends Controller
         ->join('penyedia_jasa_mua', 'penyedia_jasa_mua.id', '=', 'layanan.penyedia_jasa_mua_id')
         ->leftJoin('galeri_pembeli', 'galeri_pembeli.ulasan_id', '=', 'ulasan.id') // Left join to include cases where there's no galeri_pembeli entry
         ->where('layanan.penyedia_jasa_mua_id', auth()->user()->penyedia_jasa_mua->id)
-        ->where('ulasan.id', '=' , $id)
         ->where('pemesanan.status', '=', 'done')
-        ->select('pemesanan.id', 'pemesanan.tanggal_pemesanan', 'pencari_jasa_mua.nama as nama', 'pencari_jasa_mua.foto as foto', 'galeri_pembeli.foto as foto_ulasan', 'ulasan.rating', 'ulasan.komentar', 'layanan.nama as nama_layanan', 'pencari_jasa_mua.user_id', 'penyedia_jasa_mua.user_id as user_id_mua', 'penyedia_jasa_mua.nama as nama_mua')
+        ->select('pemesanan.id as pesanan_id', 'pencari_jasa_mua.user_id', 'pencari_jasa_mua.nama as nama', 'pemesanan.tanggal_pemesanan', 'layanan.nama as nama_layanan', 'ulasan.rating', 'ulasan.komentar', 'galeri_pembeli.foto as foto_ulasan', 'penyedia_jasa_mua.user_id as user_id_mua', 'penyedia_jasa_mua.nama as nama_mua')
         ->get();
 
+        $groupedData = [];
+
+        foreach ($data as $key => $value) {
+            $id = $value->id;
+            $tanggal_pemesanan = date('d-m-Y', strtotime($value->tanggal_pemesanan));
+            $foto = $this->formatFotoUrl($value);
+            $foto_ulasan = $value->foto_ulasan; // Assume this is an array of photo filenames
+    
+            // Check if the entry for this ID already exists in the grouped data
+            if (!isset($groupedData[$id])) {
+                $groupedData[$id] = [
+                    'id' => $id,
+                    'tanggal_pemesanan' => $tanggal_pemesanan,
+                    'nama' => $value->nama,
+                    'foto' => $foto,
+                    'foto_ulasan' => [],
+                    'rating' => $value->rating,
+                    'nama_layanan' => $value->nama_layanan,
+                    'komentar' => $value->komentar,
+                    'user_id' => $value->user_id,
+                ];
+            }
+    
+            // Add the review photo to the grouped data
+            if (!empty($foto_ulasan)) {
+                $groupedData[$id]['foto_ulasan'][] = url('/file/' . $value->user_id_mua . '_' . $value->nama_mua . '/review/' . $foto_ulasan);
+            }
+        }
+    
+        // Convert associative array to indexed array
+        $groupedData = array_values($groupedData);
+    
         return response()->json([
-            'status' => 'success',
-            'message' => 'Berhasil menampilkan detail ulasan',
-            'data' => $data,
+            'success' => true,
+            'message' => 'Berhasil mendapatkan detil  ulasan',
+            'data' => $groupedData
         ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -50,6 +50,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/penyedia-jasa-mua/katalog/updatekatalogjasa', 'API\Manajemenkatalog\ManajemenKatalogController@updateKatalogJasa');
     Route::get('/penyedia-jasa-mua/katalog/kategorilayanan', 'API\Manajemenkatalog\ManajemenKatalogController@getKategoriLayanan');
     Route::post('/penyedia-jasa-mua/katalog/createkatalogjasa', 'API\Manajemenkatalog\ManajemenKatalogController@createKatalogJasa');
+    
+    // Route Seluruh Pesanan dan Detil Pesanan
+    Route::get('/penyedia-jasa-mua/ulasan/seluruhulasan', 'API\Ulasan\UlasanController@getSeluruhUlasan');
 });
 
 
