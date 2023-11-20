64e9fa0c48d9f0e6402d6c2ab8f3ffdcbbd329fd
crooz13 <zee.indigo5@gmail.com>
2023-11-20 21:36:18 +0700

    membuat directory ulasan + ulasan controller dan routenya

--- app/Http/Controllers/API/Dashboard/DashboardMuaController.php
+++ app/Http/Controllers/API/Dashboard/DashboardMuaController.php
@@ -57,9 +57,10 @@ class DashboardMuaController extends Controller
             ->join('layanan', 'layanan.id', '=', 'detail_pemesanan.layanan_id')
             ->join('kategori_layanan', 'kategori_layanan.id', '=', 'layanan.kategori_layanan_id')
             ->join('pencari_jasa_mua', 'pencari_jasa_mua.id', '=', 'pemesanan.pencari_jasa_mua_id')
+            ->join('kecamatan', 'kecamatan.id', '=', 'pencari_jasa_mua.alamat')
             ->where('pemesanan.penyedia_jasa_mua_id', auth()->user()->penyedia_jasa_mua->id)
             ->where('pemesanan.status', '=', 'pending')
-            ->selectRaw('pemesanan.id, pemesanan.tanggal_pemesanan, kategori_layanan.nama as kategori, pencari_jasa_mua.nama as nama_pencari, pencari_jasa_mua.foto as foto, pemesanan.status, pencari_jasa_mua.user_id, pencari_jasa_mua.nama')
+            ->selectRaw('pemesanan.id, pemesanan.tanggal_pemesanan, kategori_layanan.nama as kategori, pencari_jasa_mua.nama as nama_pencari, pencari_jasa_mua.foto as foto, pemesanan.status, pencari_jasa_mua.user_id, kecamatan.nama_kecamatan as alamat, pencari_jasa_mua.nama')
             ->get();
     
         foreach ($data as $key => $value) {
@@ -103,21 +104,50 @@ class DashboardMuaController extends Controller
             ->join('layanan', 'layanan.id', '=', 'detail_pemesanan.layanan_id')
             ->join('ulasan', 'ulasan.pemesanan_id', '=', 'pemesanan.id')
             ->join('pencari_jasa_mua', 'pencari_jasa_mua.id', '=', 'pemesanan.pencari_jasa_mua_id')
-            ->join('galeri_pembeli', 'galeri_pembeli.ulasan_id', '=', 'ulasan.id')
+            ->join('penyedia_jasa_mua', 'penyedia_jasa_mua.id', '=', 'layanan.penyedia_jasa_mua_id')
+            ->leftJoin('galeri_pembeli', 'galeri_pembeli.ulasan_id', '=', 'ulasan.id') // Left join to include cases where there's no galeri_pembeli entry
             ->where('layanan.penyedia_jasa_mua_id', auth()->user()->penyedia_jasa_mua->id)
             ->where('pemesanan.status', '=', 'done')
-            ->selectRaw('pemesanan.id, pemesanan.tanggal_pemesanan, pencari_jasa_mua.nama as nama_pencari, pencari_jasa_mua.foto as foto, galeri_pembeli.foto as foto_ulasan, ulasan.rating, ulasan.komentar, pencari_jasa_mua.user_id, pencari_jasa_mua.nama')
+            ->select('pemesanan.id', 'pemesanan.tanggal_pemesanan', 'pencari_jasa_mua.nama as nama', 'pencari_jasa_mua.foto as foto', 'galeri_pembeli.foto as foto_ulasan', 'ulasan.rating', 'ulasan.komentar', 'layanan.nama as nama_layanan', 'pencari_jasa_mua.user_id', 'penyedia_jasa_mua.user_id as user_id_mua', 'penyedia_jasa_mua.nama as nama_mua')
+            ->limit(3)
             ->get();
     
+        $groupedData = [];
+    
         foreach ($data as $key => $value) {
-            $data[$key]->tanggal_pemesanan = date('d-m-Y', strtotime($value->tanggal_pemesanan));
-            $data[$key]->foto = $this->formatFotoUrl($value);
+            $id = $value->id;
+            $tanggal_pemesanan = date('d-m-Y', strtotime($value->tanggal_pemesanan));
+            $foto = $this->formatFotoUrl($value);
+            $foto_ulasan = $value->foto_ulasan; // Assume this is an array of photo filenames
+    
+            // Check if the entry for this ID already exists in the grouped data
+            if (!isset($groupedData[$id])) {
+                $groupedData[$id] = [
+                    'id' => $id,
+                    'tanggal_pemesanan' => $tanggal_pemesanan,
+                    'nama' => $value->nama,
+                    'foto' => $foto,
+                    'foto_ulasan' => [],
+                    'rating' => $value->rating,
+                    'nama_layanan' => $value->nama_layanan,
+                    'komentar' => $value->komentar,
+                    'user_id' => $value->user_id,
+                ];
+            }
+    
+            // Add the review photo to the grouped data
+            if (!empty($foto_ulasan)) {
+                $groupedData[$id]['foto_ulasan'][] = url('/file/' . $value->user_id_mua . '_' . $value->nama_mua . '/review/' . $foto_ulasan);
+            }
         }
+    
+        // Convert associative array to indexed array
+        $groupedData = array_values($groupedData);
     
         return response()->json([
             'success' => true,
-            'message' => 'Berhasil mendapatkan data',
-            'data' => $data
+            'message' => 'Berhasil mendapatkan ulasan',
+            'data' => $groupedData
         ]);
     }
 }
---
+++ app/Http/Controllers/API/Ulasan/UlasanController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API\Ulasan;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Layanan;
+use App\Models\Pemesanan;
+use App\Models\PenyediaJasaMua;
+use Illuminate\Support\Facades\DB;
+
+class UlasanController extends Controller
+{
+    public function getSeluruhUlasan()
+    {
+        $data = Pemesanan::join('detail_pemesanan', 'detail_pemesanan.pemesanan_id', '=', 'pemesanan.id')
+        ->join('layanan', 'layanan.id', '=', 'detail_pemesanan.layanan_id')
+        ->join('ulasan', 'ulasan.pemesanan_id', '=', 'pemesanan.id')
+        ->join('pencari_jasa_mua', 'pencari_jasa_mua.id', '=', 'pemesanan.pencari_jasa_mua_id')
+        ->join('penyedia_jasa_mua', 'penyedia_jasa_mua.id', '=', 'layanan.penyedia_jasa_mua_id')
+        ->leftJoin('galeri_pembeli', 'galeri_pembeli.ulasan_id', '=', 'ulasan.id') // Left join to include cases where there's no galeri_pembeli entry
+        ->where('layanan.penyedia_jasa_mua_id', auth()->user()->penyedia_jasa_mua->id)
+        ->where('pemesanan.status', '=', 'done')
+        ->select('pemesanan.id', 'pemesanan.tanggal_pemesanan', 'pencari_jasa_mua.nama as nama', 'pencari_jasa_mua.foto as foto', 'galeri_pembeli.foto as foto_ulasan', 'ulasan.rating', 'ulasan.komentar', 'layanan.nama as nama_layanan', 'pencari_jasa_mua.user_id', 'penyedia_jasa_mua.user_id as user_id_mua', 'penyedia_jasa_mua.nama as nama_mua')
+        ->get();
+
+    $groupedData = [];
+
+    foreach ($data as $key => $value) {
+        $id = $value->id;
+        $tanggal_pemesanan = date('d-m-Y', strtotime($value->tanggal_pemesanan));
+        $foto = $this->formatFotoUrl($value);
+        $foto_ulasan = $value->foto_ulasan; // Assume this is an array of photo filenames
+
+        // Check if the entry for this ID already exists in the grouped data
+        if (!isset($groupedData[$id])) {
+            $groupedData[$id] = [
+                'id' => $id,
+                'tanggal_pemesanan' => $tanggal_pemesanan,
+                'nama' => $value->nama,
+                'foto' => $foto,
+                'foto_ulasan' => [],
+                'rating' => $value->rating,
+                'nama_layanan' => $value->nama_layanan,
+                'komentar' => $value->komentar,
+                'user_id' => $value->user_id,
+            ];
+        }
+
+        // Add the review photo to the grouped data
+        if (!empty($foto_ulasan)) {
+            $groupedData[$id]['foto_ulasan'][] = url('/file/' . $value->user_id_mua . '_' . $value->nama_mua . '/review/' . $foto_ulasan);
+        }
+    }
+
+    // Convert associative array to indexed array
+    $groupedData = array_values($groupedData);
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Berhasil mendapatkan seluruh ulasan',
+        'data' => $groupedData
+    ]);
+    }
+
+    public function getDetaillUlasan($id)
+    {
+        $data = Pemesanan::join('detail_pemesanan', 'detail_pemesanan.pemesanan_id', '=', 'pemesanan.id')
+        ->join('layanan', 'layanan.id', '=', 'detail_pemesanan.layanan_id')
+        ->join('ulasan', 'ulasan.pemesanan_id', '=', 'pemesanan.id')
+        ->join('pencari_jasa_mua', 'pencari_jasa_mua.id', '=', 'pemesanan.pencari_jasa_mua_id')
+        ->join('penyedia_jasa_mua', 'penyedia_jasa_mua.id', '=', 'layanan.penyedia_jasa_mua_id')
+        ->leftJoin('galeri_pembeli', 'galeri_pembeli.ulasan_id', '=', 'ulasan.id') // Left join to include cases where there's no galeri_pembeli entry
+        ->where('layanan.penyedia_jasa_mua_id', auth()->user()->penyedia_jasa_mua->id)
+        ->where('ulasan.id', '=' , $id)
+        ->where('pemesanan.status', '=', 'done')
+        ->select('pemesanan.id', 'pemesanan.tanggal_pemesanan', 'pencari_jasa_mua.nama as nama', 'pencari_jasa_mua.foto as foto', 'galeri_pembeli.foto as foto_ulasan', 'ulasan.rating', 'ulasan.komentar', 'layanan.nama as nama_layanan', 'pencari_jasa_mua.user_id', 'penyedia_jasa_mua.user_id as user_id_mua', 'penyedia_jasa_mua.nama as nama_mua')
+        ->get();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Berhasil menampilkan detail ulasan',
+            'data' => $data,
+        ], 200);
+    }
+}
