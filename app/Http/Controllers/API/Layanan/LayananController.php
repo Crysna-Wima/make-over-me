47ffbb93a6d84adb928a2784859fd38e1b6c9525
Crysna-Wima <wcrysna@gmail.com>
2023-11-22 15:41:20 +0700

    update api

--- app/Http/Controllers/API/Layanan/LayananController.php
+++ app/Http/Controllers/API/Layanan/LayananController.php
@@ -18,4 +18,34 @@ class LayananController extends Controller
             'data' => $layanan,
         ]);
     }
+
+    public function getAllLayananMua()
+    {
+        $layanan = Layanan::select('layanan.id', 'layanan.nama', 'layanan.foto', 'penyedia_jasa_mua.nama_jasa_mua', 'kecamatan.nama_kecamatan as lokasi', 'penyedia_jasa_mua.nama', 'users.id as user_id', 'penyedia_jasa_mua.id as id_mua')
+        ->leftJoin('detail_pemesanan', 'layanan.id', '=', 'detail_pemesanan.layanan_id')
+        ->leftJoin('ulasan', 'detail_pemesanan.pemesanan_id', '=', 'ulasan.pemesanan_id')
+        ->leftJoin('penyedia_jasa_mua', 'layanan.penyedia_jasa_mua_id', '=', 'penyedia_jasa_mua.id')
+        ->leftJoin('kecamatan', 'penyedia_jasa_mua.lokasi_jasa_mua', '=', 'kecamatan.id')
+        ->leftJoin('users', 'penyedia_jasa_mua.user_id', '=', 'users.id')
+        ->get();
+
+        foreach ($layanan as $key => $value) {
+            $value->foto = $this->formatLayananUrl($value);
+            $value->lokasi = $value->lokasi.', Surabaya';
+        }
+
+        $data = [];
+        foreach ($layanan as $key => $value) {
+            $data[$key]['id'] = $value->id_mua;
+            $data[$key]['nama'] = $value->nama_jasa_mua;
+            $data[$key]['foto'] = $value->foto;
+            $data[$key]['lokasi'] = $value->lokasi;
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Berhasil mendapatkan data layanan',
+            'data' => $data,
+        ]);
+    }
 }
